d1d78bc2f8d17877ac2095de46da0af540363e0f
Shachar Ben-David <shacharb@wizards.co.il>
2024-12-12 11:37:18 +0200

    networking done

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_virtual_network" "vm_network" {
 # Create subnet
 resource "azurerm_subnet" "vm_subnet" {
   name                 = "${var.resource_name_prefix}-vm-subnet"
-  resource_group_name = var.resource_group_name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vm_network.name
   address_prefixes     = ["10.3.1.0/24"]
 }
@@ -38,10 +38,55 @@ resource "azurerm_public_ip" "jenkins_public_ip" {
 }
 
 # Create NSG and rule
+resource "azurerm_network_security_group" "jenkins_nsg" {
+  name                = "${var.resource_name_prefix}-nsg-jenkins"
+  location            = var.resources_location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Jenkins-8080"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
 
 # Create NIC
+resource "azurerm_network_interface" "jenkins_nic" {
+  name                = "${var.resource_name_prefix}-nic-jenkins"
+  location            = var.resources_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "${var.resource_name_prefix}-ip-jenkins"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.jenkins_public_ip.id
+  }
+}
 
 # Connect the NSG to NIC
+resource "azurerm_network_interface_security_group_association" "nsg_to_nic" {
+  network_interface_id      = azurerm_network_interface.jenkins_nic.id
+  network_security_group_id = azurerm_network_security_group.jenkins_nsg.id
+}
 
 # Create virtual machine
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,15 @@
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
+output "vm_virtual_network" {
+  value = azurerm_virtual_network.vm_network.name
+}
+
+output "vm_subnet" {
+  value = azurerm_subnet.vm_subnet.name
+}
+
+output "jenkins_IP_address" {
+  value = azurerm_network_interface.jenkins_nic.ip_configuration[0].public_ip_address_id
+}
