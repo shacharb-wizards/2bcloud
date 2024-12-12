bda938c4753d2840d539c483b49ef0890b3688fc
Shachar Ben-David <shacharb@wizards.co.il>
2024-12-12 15:03:42 +0200

    adding docker

--- terraform/azure-custom-data.sh
+++ terraform/azure-custom-data.sh
@@ -1,9 +1,33 @@
 #! /bin/bash
+# prepare for Jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# deployments:
 sudo apt update
 sudo apt install fontconfig openjdk-17-jre -y
 sudo apt-get install jenkins -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+#Grant Jenkins user and Ubuntu user permission to docker deamon:
+sudo usermod -aG docker jenkins;
+sudo usermod -aG docker azureadmin;
+
+# reboot after all changes:
+sudo shutdown -r now
