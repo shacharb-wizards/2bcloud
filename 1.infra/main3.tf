9c5cad1019fab57b1ba60c9775626b465c3001e5
Shachar Ben-David <shacharb@wizards.co.il>
2024-12-16 11:51:13 +0200

    ingress fixes

--- 1.infra/main3.tf
+++ 1.infra/main3.tf
@@ -16,14 +16,14 @@ resource "helm_release" "redis-sentinel" {
 # create DNS zone
 resource "azurerm_dns_zone" "zone" {
   name = var.dns_zone_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = "MC_${var.resource_group_name}_${azurerm_kubernetes_cluster.k8s.name}_${var.resources_location}"
 }
 
 # create static IP for Ingress
 resource "azurerm_public_ip" "ingress_public_ip" {
   name                = "ingress_public_ip"
   location            = var.resources_location
-  resource_group_name = var.resource_group_name
+  resource_group_name = "MC_${var.resource_group_name}_${azurerm_kubernetes_cluster.k8s.name}_${var.resources_location}"
   allocation_method   = "Static"
   sku                 = "Standard"
   depends_on          = [azurerm_public_ip.ingress_public_ip]
@@ -32,7 +32,7 @@ resource "azurerm_public_ip" "ingress_public_ip" {
 # Register ingress_public_ip in DNS
 resource "azurerm_dns_a_record" "ingress_record" {
   name                = "www"
-  resource_group_name = var.resource_group_name
+  resource_group_name = "MC_${var.resource_group_name}_${azurerm_kubernetes_cluster.k8s.name}_${var.resources_location}"   # rg should be the one that nodes are using (Virtual machine scale set)
   zone_name           = azurerm_dns_zone.zone.name
   ttl                 = var.dns_ttl
   records             = [azurerm_public_ip.ingress_public_ip.ip_address]
@@ -48,5 +48,9 @@ resource "helm_release" "ingress-nginx" {
   version          = "4.11.3"
   depends_on          = [azurerm_kubernetes_cluster.k8s, azurerm_dns_a_record.ingress_record]
   
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = "${azurerm_public_ip.ingress_public_ip.ip_address}"
+  }  
   values = [file("${path.module}/ingress.yaml")]
 }
